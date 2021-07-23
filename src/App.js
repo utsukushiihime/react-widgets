fb3859e071db14400400127c699df1c9b8c0b5c9
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-22 20:07:26 -0500

    adds bootstrap and sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+	{
+		title: "Accordion Item 1",
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	},
+	{
+		title: "Accordion Item 2",
+		content: "Lorem ipsum dolor sit amet, this is some more content.",
+	},
+	{
+		title: "Accordion Item 3",
+		content: "Lorem ipsum dolor sit amet, this is some more content.",
+	},
+];
+
+const App = () => {
+	return (
+		<div>
+			<Accordion items={items} />
+		</div>
+	);
+};
 
 export default App;
