75d03493984d11fc6c389f967d39594012ed6a80
Crystal McNeil <crystal.mcneil@creativarian.com>
2021-07-24 15:46:10 -0500

    adds search debounce

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('algorithm');
+  const [term, setTerm] = useState('algorithms');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerID);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
@@ -13,25 +24,13 @@ const Search = () => {
           list: 'search',
           format: 'json',
           origin: '*',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
-
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   // return results
   const renderedResults = results.map((result) => {
